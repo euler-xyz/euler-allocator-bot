d93f3230872d3283ac1f03bca17e5062c2352d6f
dglowinski <dariushg32111@gmail.com>
2025-10-03 20:49:24 +0200

    add plasma chain config

--- src/utils/common/chainConversion.ts
+++ src/utils/common/chainConversion.ts
@@ -1,4 +1,33 @@
+import { defineChain } from 'viem';
 import { arbitrum, base, mainnet } from 'viem/chains';
+
+
+export const plasma = defineChain({
+  id: 9745,
+  name: "Plasma",
+  nativeCurrency: {
+    decimals: 18,
+    name: "XPL",
+    symbol: "XPL",
+  },
+  blockExplorers: {
+    default: {
+      name: "Plasma Explorer",
+      url: "https://plasmascan.to/",
+    },
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.plasma.to"],
+    },
+  },
+  contracts: {
+    multicall3: {
+      address: "0xcA11bde05977b3631167028862bE2a173976CA11",
+      blockCreated: 0,
+    },
+  },
+} as const)
 
 /**
  * @notice Get the appropriate chain configuration based on chainId
@@ -14,6 +43,8 @@ export function getChain(chainId: number) {
       return base;
     case 42161:
       return arbitrum;
+    case 9745:
+      return plasma;
     default:
       throw new Error(`Unsupported chainId: ${chainId}`);
   }
